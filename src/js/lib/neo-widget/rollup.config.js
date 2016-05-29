539d7b3eaef6158c826b712d7f3db3b0fabc0aad
Noel Delgado <auhcsei@gmail.com>
2016-05-29 14:32:13 -0500

    add / jsnext:mainc

--- src/js/lib/neo-widget/rollup.config.js
+++ src/js/lib/neo-widget/rollup.config.js
@@ -1,9 +1,21 @@
 import buble from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'index.js',
   format: 'umd',
   moduleName: 'NeoWidget',
-  plugins: [buble()],
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    buble(),
+    uglify(),
+  ],
   dest: './lib/index.js',
 };
